2a4b13ba90c3dca0760d6891f6bc32038c3da2b7
JOEV <josejaredvillatorovargas@gmail.com>
2025-07-30 15:48:49 -0600

    msg el

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const promptInput = document.querySelector(".prompt-input");
 
 let userMessage = "";
 
+const createMsgElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+    div.innerHTML = content;
+    return div;
+}
+
 const handlenFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
 
     if(!userMessage) return;
 
-    console.log(userMessage)
+    const userMsgHTML = `<p class="message-text"></p>`
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message")
 }
 
 promptForm.addEventListener("submit" , handlenFormSubmit);
