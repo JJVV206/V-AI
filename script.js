eb4501aac7e0a493878f46217548c92ab86c93ff
JOEV <josejaredvillatorovargas@gmail.com>
2025-07-30 18:56:30 -0600

    animation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const chatsContainer = document.querySelector(".chats-conainer");
+const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
 let userMessage = "";
 
-const createMsgElement = (content, className) => {
+const createMsgElement = (content, ...classes) => {
     const div = document.createElement("div");
-    div.classList.add("message", className);
+    div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
 }
@@ -16,13 +16,19 @@ const handlenFormSubmit = (e) => {
     userMessage = promptInput.value.trim();
     if(!userMessage) return;
 
-    console.log(userMessage)
+    promptInput.value = "";
 
     const userMsgHTML = `<p class="message-text"></p>`;
     const userMsgDiv = createMsgElement(userMsgHTML, "user-message")
 
     userMsgDiv.querySelector(".message-text").textContent = userMessage;
     chatsContainer.appendChild(userMsgDiv)
+
+    setTimeout(() => {
+        const botMsgHTML = `<img src="gemini-logo.webp" alt="gemini-logo" class="avatar"><p class="message-text">Just a sec...</p>`;
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading")
+        chatsContainer.appendChild(botMsgDiv) 
+    }, 600)
 }
 
 promptForm.addEventListener("submit", handlenFormSubmit);
